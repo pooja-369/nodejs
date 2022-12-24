703024a6b7f5fa19ea25edbfde8c4cfb4ce9af9f
pooja-369 <pujagoyal369@gmail.com>
2022-12-24 21:13:31 +0530

    install nodemon and yargs packages

--- node-app/app.js
+++ node-app/app.js
@@ -1,6 +1,41 @@
 const fs=require('fs')
 const load =require('./utils')
+const yargs=require('yargs')
 fs.writeFileSync('note2.txt','this is the file created by nodejs')
 console.log(load.name)
-const sum =load(4,2)
+const sum =load(8,2)
 console.log(sum)
+
+//command line argument default 
+console.log(process.argv)
+const command =process.argv[2]
+if(command==='add'){
+    console.log('adding a note ')
+}else if(command==='remove'){
+    console.log('reomving a note')
+}
+
+//command line argument using yargs
+
+//create add command 
+yargs.command({
+    command:'add',
+    describe:'adding a note',
+    handler:function(){
+        console.log('adding a new note !')
+    }
+})
+
+//create remove command 
+
+yargs.command({
+    command:'remove',
+    describe:'removing a note',
+    handler:function(){
+        console.log('removing a note !')
+    }
+})
+
+console.log(yargs.argv) //must be after otherwise handler didn't run 
+
+
